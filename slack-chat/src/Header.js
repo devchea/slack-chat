715112460c8ae71de3c36fad44b2ada2c2dfff3d
devchea <rchea817@gmail.com>
2020-10-04 20:33:42 -0700

    create header

---
+++ slack-chat/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatar for logged in user */}
+        {/* Time Icon */}
+      </div>
+      <div className="header__search">
+        {/* Search Icon */}
+        {/* Input */}
+      </div>
+      <div className='header__right'>
+        {/* Help Icon */}
+      </div>
+    </div>
+  );
+}
