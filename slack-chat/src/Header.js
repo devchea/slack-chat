ba7c6288f3362fddd34dfe8ab213dc70296b4a6b
devchea <rchea817@gmail.com>
2020-10-04 20:45:44 -0700

    updated header with avatar

--- slack-chat/src/Header.js
+++ slack-chat/src/Header.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
 
 function Header() {
   return (
-    <div className="header">
-      <div className="header__left">
+    <div className='header'>
+      <div className='header__left'>
+        <Avatar
+          className='header_avater'
+          alt={"Me"}
+          // src={}
+          />
+        <AccessTimeIcon />
         {/* Avatar for logged in user */}
         {/* Time Icon */}
       </div>
-      <div className="header__search">
+      <div className='header__search'>
         {/* Search Icon */}
         {/* Input */}
       </div>
@@ -18,3 +26,5 @@ function Header() {
     </div>
   );
 }
+
+export default Header
