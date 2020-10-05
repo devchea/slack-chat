0d6c1d83900e0cb370ef801562debd527e94ebdf
devchea <rchea817@gmail.com>
2020-10-04 21:05:54 -0700

    added sidebar

--- slack-chat/src/App.js
+++ slack-chat/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <Header />
+      <div className='app__body'>
+      <Sidebar />
+      </div>
     </div>
   );
 }
